bc0bb1a059d208e839dbd567dcf61d701b4b84c6
Dmitry Holodtsov <holodtsovd@gmail.com>
2020-07-17 01:25:01 +0300

    Homework lesson2

--- lesson02/src/Task4CountPositive.java
+++ lesson02/src/Task4CountPositive.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+//Даны 3 целых числа. Найти количество положительных чисел в исходном наборе.
+
+import java.util.Scanner;
 
 public class Task4CountPositive {
+    public static void main(String[] args) {
+        System.out.println("Введите 3 целых числа:");
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+        int count = 0;
+        if (n1 > 0)
+            count++;
+        if (n2 > 0)
+            count++;
+        if (n3 > 0)
+            count++;
+        System.out.println("Количество положительных чисел - " + count);
+
+    }
 }
--- lesson02/src/Task6GreaterNumber.java
+++ lesson02/src/Task6GreaterNumber.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+// Даны 2 числа. Вывести большее из них.
+
+import java.util.Scanner;
 
 public class Task6GreaterNumber {
+    public static void main(String[] args) {
+
+        System.out.println("Введите 2 целых числа:");
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        if (n1 != n2)
+            if (n1 > n2)
+                System.out.println("Большее число " + n1);
+            else
+                System.out.println("Большее число " + n2);
+
+    }
 }
