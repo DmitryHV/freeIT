a1f6b3feda185f0465fcf75851dd60edd16f45e2
Dmitry Holodtsov <holodtsovd@gmail.com>
2020-08-25 02:41:09 +0300

    save052

--- FreeITCourse/src/lesson05/MetodTask22.java
+++ FreeITCourse/src/lesson05/MetodTask22.java
@@ -6,6 +6,11 @@ package lesson05;
 
 public class MetodTask22 {
     public static void main(String[] args) {
-
+//        String str = "1C3B3";
+//        String str2 = "9E73C";
+//        String str3 = "9E73K";
+//        System.out.println(str.matches("[0-9ABCDEF]+\\d"));
+//        System.out.println(str2.matches("[0-9A-F]+\\(d| W)"));
+//        System.out.println(str3.matches("[0-9A-F]+\\d"));
     }
 }
