5f2c8ff0fe6aab4fb4104621782ba08ae6897236
Dmitry Holodtsov <holodtsovd@gmail.com>
2020-09-22 01:24:53 +0300

    save072 Task24 is finished

--- FreeITCourse/src/lesson07/MetodTask24.java
+++ FreeITCourse/src/lesson07/MetodTask24.java
@@ -4,7 +4,7 @@ package lesson07;
 // Сам промежуток в классе должен задаваться тремя свойствами: секундами, минутами, часами.
 // 1. Сделать методы для получения полного количества секунд в объекте, сравнения
 // двух объектов (метод должен работать аналогично compareTo в строках).
-// +2. Создать два конструктора: получающий общее количество секунд, и часы, минуты и секунды по отдельности.
+// 2. Создать два конструктора: получающий общее количество секунд, и часы, минуты и секунды по отдельности.
 // 3. Сделать метод для вывода данных.
 
 
@@ -14,10 +14,28 @@ public class MetodTask24 {
         TimeInterval t1 = new TimeInterval(25000);
         TimeInterval t2 = new TimeInterval(3, 24, 45); // 12285 sec
 
+// получаем полное количество секунд
+
         System.out.println("Полное количество секунд t1 = " + t1.getTotalSec());
         System.out.println("Полное количество секунд t2 = " + t2.getTotalSec());
-        t1.getData();
-        t2.getData();
+
+// выводим данные
+
+        System.out.println("Выводим параметры t1:");
+        t1.printData();
+        System.out.println("Выводим параметры t2:");
+        t2.printData();
+
+// сравниваем объекты
+// если объект вызывающий больше, то выводим 1
+// если объект вызывающий меньше, то выводим -1
+// если объекты равны, то выводим 0
+
+        System.out.println("Сравниваем объекты:");
+        TimeInterval t3 = new TimeInterval(25000);
+        System.out.println("t1 = " + t1.getTotalSec() + " с, t2 = " + t2.getTotalSec() + " с, результат: " + t1.compareTime(t2));
+        System.out.println("t2 = " + t2.getTotalSec() + " с, t1 = " + t1.getTotalSec() + " с, результат: " + t2.compareTime(t1));
+        System.out.println("t3 = " + t3.getTotalSec() + " с, t1 = " + t1.getTotalSec() + " с, результат: " + t3.compareTime(t1));
 
     }
 
--- FreeITCourse/src/lesson07/TimeInterval.java
+++ FreeITCourse/src/lesson07/TimeInterval.java
@@ -2,29 +2,35 @@ package lesson07;
 
 public class TimeInterval {
 
-        private int sec;
-        private int min;
-        private int hour;
+    private int sec;
+    private int min;
+    private int hour;
 
-        public TimeInterval (int sec) {
-            this.sec = sec;
-        }
+    public TimeInterval(int sec) {
+        this.sec = sec;
+    }
 
-        public TimeInterval (int hour, int min, int sec) {
-            this.hour = hour;
-            this.min = min;
-            this.sec = sec;
-        }
+    public TimeInterval(int hour, int min, int sec) {
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+    }
 
-        public int getTotalSec () {
-            int totalSec = hour * 3600 + min * 60 + sec;
-            return totalSec;
-        }
+    public int getTotalSec() {
+        return hour * 3600 + min * 60 + sec;
+    }
 
-        public void getData () {
-        System.out.println(hour + " hour(s), " + min + " min(es), " + sec + " sec(s).");
-//            return str;
-        }
+    public void printData() {
+        System.out.println("hour = " + hour + "; \n" + "min = " + min + "; \n" + "sec = " + sec + ";");
+    }
+
+    public int compareTime(TimeInterval T) {
+        int totalSec1 = this.hour * 60 * 60 + this.min * 60 + this.sec;
+        int totalSec2 = T.hour * 60 * 60 + T.min *60 + T.sec;
+        if (totalSec1 == totalSec2)
+            return  0;
+        else return totalSec1 > totalSec2 ? 1 : -1;
+    }
 
 
 }
